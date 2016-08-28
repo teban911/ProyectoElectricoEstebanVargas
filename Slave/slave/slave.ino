d74cb13f1d94e616782feefcf9388df482c0a1f5
Esteban Vargas <Esteban Vargas>
2016-08-27 21:49:06 -0600

    Prototipo del Slave

---
+++ Slave/slave/slave.ino
@@ -0,0 +1,56 @@
+/* Proyecto Eléctrico IE-0502
+    Diseño e implementación de un sistema de alarmas multisensoriales
+    Estudiante: Esteban Vargas Arrieta
+    Carné: A96486
+    Profesor guía: MSc. Fabian Abarca Calderón
+    Lectores: Lic. Carlos Nájera Guadamuz
+              Dr. Jaime Cascante Vindas
+*/
+
+// Variables
+int rojo = 13;
+int amarillo = 12;
+int motor = 11;
+
+void setup() {
+  // Se definen los I/O del sistema incluyendo el puerto serial
+  pinMode(rojo, OUTPUT);
+  pinMode(amarillo, OUTPUT);
+  pinMode(motor, OUTPUT);
+  Serial.begin(9600);
+}
+
+// Este loop contiene las instrucciones que se ejecutaran segun la instruccion leida a traves del puerto serial
+void loop() {
+  // Se apagan los leds y motores en caso de no estarse utilizando
+  digitalWrite(rojo, LOW);
+  digitalWrite(amarillo, LOW);
+  while (Serial.available()) {
+    char mensaje = Serial.read();
+    // Se comprueba el dato y se selecciona el estado segun corresponda
+    switch (mensaje) {
+      case 'a':{ 
+        for (int i = 0; i<=20; i++) {
+          digitalWrite(rojo, HIGH);
+          digitalWrite(motor, HIGH);
+          delay(80);
+          digitalWrite(rojo, LOW);
+          digitalWrite(rojo, LOW);
+          delay(80);
+        }
+        break;
+      }
+      case 'b':{
+        for (int i=0; i<=20; i++){
+          digitalWrite(amarillo, HIGH);
+          digitalWrite(motor, HIGH);
+          delay(120);
+          digitalWrite(amarillo, LOW);
+          digitalWrite(motor, LOW);
+          delay(60);
+        }
+        break;
+      }
+    }
+  }
+}

0a2bd3ab14caab263f1027684bdd4669e37dbdad
Esteban Vargas <estebanva911@gmail.com>
2016-11-12 00:58:20 -0600

    se implementaron todos los sensores y modos de operacion

--- Master/master/master.ino
+++ Master/master/master.ino
@@ -11,9 +11,13 @@
 // Se definen los nuevos puertos seriales en el orden Rx, Tx
 SoftwareSerial MySerial(13, 12); //Rx,Tx
 // Variables
-int pir = 11;
-int button = 10;
+const int pir = 11;
+const int button = 10;
+const int sound = 9;
+const int gas = A0;
 int pirState = LOW;
+int gasState = LOW;
+int gasValue = 0;
 int buttonState = 0;
 int lastButtonState = 0;
 unsigned long preMillis = 0;
@@ -25,15 +29,35 @@ void setup() {
   MySerial.begin(9600);
   pinMode(pir, INPUT);
   pinMode(button, INPUT);
+  pinMode(sound, HIGH);
 }
 
 void loop() {
   //Los datos se transmiten en el siguiente orden:
-  // (R,G,B,tTotal,thL,tlL,thM,tlM)
-  if (digitalRead(pir) == HIGH) { //Se lee el valor del PIR
+  // (R,G,B,tTotal,thL,tlL,thM,tlM,Despertador,Sonido) //Nota: poner siempre la coma "," al final, pues se necesita para saber donde termina el string
+  /////////////////////////////////////////////////////////////////////////////////////////////
+  //                    GAS sensor
+  /////////////////////////////////////////////////////////////////////////////////////////////
+  gasValue = analogRead(gas);
+  if (gasValue > 10){  //Se lee el dato del sensor de gas
+    if (gasState == LOW){
+      gasState = HIGH;
+      Serial.write("230,11,246,6000,500,400,1000,300,0,1,");
+      delay(3000);  
+    }
+  }
+  else {
+    if (gasState == HIGH){
+      gasState = LOW;
+    }
+  }
+  /////////////////////////////////////////////////////////////////////////////////////////////
+  //                    PIR sensor
+  /////////////////////////////////////////////////////////////////////////////////////////////
+  if (digitalRead(pir) == HIGH) { //Se lee el dato del sensor de movimiento
     if (pirState == LOW) {
       pirState = HIGH;
-      Serial.write("240,0,0,7000,500,400,1800,400,");
+      Serial.write("240,0,0,7000,500,400,1800,400,0,1,");
       delay(3000);
     }
   }
@@ -42,35 +66,49 @@ void loop() {
       pirState = LOW;
     }
   }
+ /////////////////////////////////////////////////////////////////////////////////////////////
+  //                   Bluetooth sensor
+  /////////////////////////////////////////////////////////////////////////////////////////////
   if (MySerial.available()) { // Se leen los datos provenientes de los puertos seriales
     char dato = MySerial.read(); //Se lee el dato proveniente del celular
     //Serial.write(MySerial.read());
     switch(dato){
       case 'w':{ // Whatsapp
-        Serial.write("20,181,17,1300,300,300,1300,0,");
+        Serial.write("20,181,17,1300,300,300,1300,0,0,0,");
       }
       break;
-      case 'f':{
-        Serial.write("19,67,179,2000,500,500,500,500,");
+      case 'f':{ //facebook
+        Serial.write("19,67,179,2000,500,500,500,500,0,0,");
       }
       break;
-      case 'g':{
-        Serial.write("200,0,0,1000,1000,1,1000,1,");
+      case 'g':{ //Gmail
+        Serial.write("200,0,0,1000,1000,1,1000,1,0,0,");
+      }
+      break;
+      case 'd':{ //Despertador
+        Serial.write("239,239,0,500,500,500,750,750,1,1,");
       }
       break;
     }
   }
-
-  //Timbre
+  /////////////////////////////////////////////////////////////////////////////////////////////
+  //                    Timbre
+  /////////////////////////////////////////////////////////////////////////////////////////////
   buttonState = digitalRead(button);
   if(buttonState != lastButtonState){
     unsigned long currentMillis = millis();
     //Se realiza la resta y hasta que no se cumpla la condicion no ejecuta la instruccion
     if((currentMillis - preMillis) > rebote){
       preMillis = currentMillis;
-      Serial.write("238,255,7,3000,800,200,3000,200,");
+      Serial.write("238,255,7,3000,800,200,3000,200,0,1,");
     }  
       lastButtonState = buttonState;
+  }
+  /////////////////////////////////////////////////////////////////////////////////////////////
+  //                    Sonido
+  /////////////////////////////////////////////////////////////////////////////////////////////
+  if(sound == HIGH){
+    Serial.write("252,127,206,300,500,500,750,750,1,1,");
   }
 }
 
--- Slave/slave/slave.ino
+++ Slave/slave/slave.ino
@@ -9,16 +9,22 @@
 //#include <SoftwareSerial.h>
 #include "FastLED.h"
 #define NUM_LEDS 3
-#define DATA_PIN 13
+#define DATA_PIN 3
 
+////////////////////////////////////////////////////////////////////////////////////////////
 // Variables
+////////////////////////////////////////////////////////////////////////////////////////////
 CRGB leds[NUM_LEDS];
 String Datos[8];
 String Msg;
-int motor = 7;
+int motor = 6;
 unsigned long tRef;
 unsigned long tRefL;
 unsigned long tRefM;
+const int parlante = 13;
+int despertar = 0;
+int boton = 10;
+int sensitividad = 0;
 
 
 void setup() {
@@ -27,6 +33,7 @@ void setup() {
   Serial.begin(9600);
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
   pinMode(motor, OUTPUT);
+  pinMode(boton, INPUT);
 }
 
 
@@ -35,7 +42,7 @@ void loop(){
   if (Serial.available()){
     //Msg = Serial.read(); //Se lee el mensaje que ha llegado
     int j = 0;
-    for(int i=0; i<=7; i++){
+    for(int i=0; i<=9; i++){
       Datos[j] = Serial.readStringUntil(',');
       j++;
     }
@@ -51,49 +58,156 @@ void loop(){
     int tlL = Datos[5].toInt();
     int thM = Datos[6].toInt();
     int tlM = Datos[7].toInt();
+    int Desp = Datos[8].toInt();
+    int Sound = Datos[9].toInt();
 
-    //Se llama la funcion alarma con los datos leídos
+    //Se escoge el tipo de funcion segun el valor de Desp, si es 1 llama la funcion despertador, si es cero llama la funcion alarma
     //alarma(Red,Green,Blue,Ciclos,th,tl);
-   alarma(Red,Green,Blue,tTotal,thL,tlL,thM,tlM); 
+    if(Desp == 0){
+      alarma(Red,Green,Blue,tTotal,thL,tlL,thM,tlM,Sound);
+    }
+    if(Desp == 1){
+      despertar = 1;
+      sensitividad = 20;
+      //despertador();
+    }
   }
 }
 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                           Funcion de alarma
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 //int alarma(int color1, int color2, int color3, int ciclos, int th, int tl) {
-int alarma(int color1, int color2, int color3, int tTotal, int thL, int tlL, int thM, int tlM) {
+int alarma(int color1, int color2, int color3, int tTotal, int thL, int tlL, int thM, int tlM, int Sound) {
   tRef = millis();
   tRefL = millis();
   tRefM = millis();
   while((millis() - tRef) < tTotal){
-  //for (int i = 0; i < ciclos; i++) {
+    ////////////////////////////////////////////////////////
+    // Enciende los LED durante el tiempo en alto de los LED
+    ////////////////////////////////////////////////////////
     if((millis()- tRefL) < thL ){ 
       leds[0] = CRGB(color1, color2, color3);
       leds[1] = CRGB(color1, color2, color3);
       leds[2] = CRGB(color1, color2, color3);
       FastLED.show();
     }
+    ////////////////////////////////////////////////////////
+    // Si se excede el tiempo en alto de los LED se apagan
+    ////////////////////////////////////////////////////////
     if(((millis()- tRefL) > thL) && ((millis()-tRefL) < (thL + tlL))){
       leds[0] = CRGB(0, 0, 0);
       leds[1] = CRGB(0, 0, 0);
       leds[2] = CRGB(0, 0, 0);
       FastLED.show();
     }
+    /////////////////////////////////////////////////////////
+    //Se refresca el tiempo de referencia de los LED
+    /////////////////////////////////////////////////////////
     if((millis() - tRefL) > (thL + tlL)){
       tRefL = millis();
     }
+    //////////////////////////////////////////////////////////////////
+    //Enciende el motor durante el tiempo en alto del motor
+    //Si Sound es igual a uno, enciende el parlante duante este tiempo
+    //////////////////////////////////////////////////////////////////
     if((millis()- tRefM) < thM ){
       analogWrite(motor,153);
+      if(Sound == 1){
+        tone(parlante, 600);
+      }
     }
+    ////////////////////////////////////////////////////////////////////
+    //Si se excede el tiempo en alto del motor se apaga junto con el mic
+    ////////////////////////////////////////////////////////////////////
     if(((millis()- tRefM) > thM) && ((millis()-tRefM) < (thM + tlM))){
       analogWrite(motor,0);
+      noTone(parlante);
     }
     if((millis() - tRefM) > (thM + tlM)){
       tRefM = millis();
     }
   }
+  ///////////////////////////////////////////////
   //Cuando sale del while apaga todas las salidas
+  ///////////////////////////////////////////////
   leds[0] = CRGB(0, 0, 0);
   leds[1] = CRGB(0, 0, 0);
   leds[2] = CRGB(0, 0, 0);
   FastLED.show();
+  noTone(parlante);
+  analogWrite(motor,0);
+}
+
+
+
+ /////////////////////////////////////////////////////////////////////////
+ /// Funcion de alarma despertadora
+ /////////////////////////////////////////////////////////////////////////
+int alarma(int color1, int color2, int color3, int thL, int tlL, int thM, int tlM, int Sound) {
+  tRef = millis();
+  tRefL = millis();
+  tRefM = millis();
+  while(despertar == 1){
+    ////////////////////////////////////////////////////////
+    // Enciende los LED durante el tiempo en alto de los LED
+    ////////////////////////////////////////////////////////
+    if(boton == HIGH){
+      despertar = 0;
+    }
+    if((millis()- tRefL) < thL ){ 
+      leds[0] = CRGB(color1, color2, color3);
+      leds[1] = CRGB(color1, color2, color3);
+      leds[2] = CRGB(color1, color2, color3);
+      FastLED.show();
+    }
+    ////////////////////////////////////////////////////////
+    // Si se excede el tiempo en alto de los LED se apagan
+    ////////////////////////////////////////////////////////
+    if(((millis()- tRefL) > thL) && ((millis()-tRefL) < (thL + tlL))){
+      leds[0] = CRGB(0, 0, 0);
+      leds[1] = CRGB(0, 0, 0);
+      leds[2] = CRGB(0, 0, 0);
+      FastLED.show();
+    }
+    /////////////////////////////////////////////////////////
+    //Se refresca el tiempo de referencia de los LED
+    /////////////////////////////////////////////////////////
+    if((millis() - tRefL) > (thL + tlL)){
+      tRefL = millis();
+    }
+    //////////////////////////////////////////////////////////////////
+    //Enciende el motor durante el tiempo en alto del motor
+    //Si Sound es igual a uno, enciende el parlante duante este tiempo
+    //////////////////////////////////////////////////////////////////
+    if((millis()- tRefM) < thM ){
+      analogWrite(motor,sensitividad);
+      if(Sound == 1){
+        tone(parlante, 600);
+      }
+    }
+    ////////////////////////////////////////////////////////////////////
+    //Si se excede el tiempo en alto del motor se apaga junto con el mic
+    ////////////////////////////////////////////////////////////////////
+    if(((millis()- tRefM) > thM) && ((millis()-tRefM) < (thM + tlM))){
+      analogWrite(motor,0);
+      noTone(parlante);
+    }
+    if((millis() - tRefM) > (thM + tlM)){
+      tRefM = millis();
+      if(sensitividad < 133){
+        sensitividad = sensitividad + 20;
+      }
+    }
+  }
+  ///////////////////////////////////////////////
+  //Cuando sale del while apaga todas las salidas
+  ///////////////////////////////////////////////
+  leds[0] = CRGB(0, 0, 0);
+  leds[1] = CRGB(0, 0, 0);
+  leds[2] = CRGB(0, 0, 0);
+  FastLED.show();
+  noTone(parlante);
   analogWrite(motor,0);
 }
